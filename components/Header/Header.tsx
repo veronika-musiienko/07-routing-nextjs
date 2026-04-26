6f40d58fa0b9ff0463e9a05f285df32e7cfffb00
Veronika <mnika5437@gmail.com>
2026-04-26 22:36:01 +0300

    fix header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,20 +1,21 @@
-import css from "../Header/Header.module.css";
+import css from "./Header.module.css"; 
 import Link from "next/link";
-import TagsMenu from "../TagsMenu/TagsMenu";
 
 export default function Header() {
   return (
     <header className={css.header}>
-      <Link href="/" aria-label="Home">
+      {/* Логотип тепер є єдиним посиланням на головну сторінку */}
+      <Link href="/" className={css.logo} aria-label="Home">
         NoteHub
       </Link>
+
       <nav aria-label="Main Navigation">
         <ul className={css.navigation}>
           <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <TagsMenu />
+            {}
+            <Link href="/notes/filter/all" className={css.link}>
+              Notes
+            </Link>
           </li>
         </ul>
       </nav>
